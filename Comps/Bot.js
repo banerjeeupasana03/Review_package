e43a156dbcd02f281753743432ba350c6dbb9a3d
Upasana Mukherjee <banerjeeupasana03@gmail.com>
2019-10-11 14:42:14 -0700

    added text input and button in bot

---
+++ Comps/Bot.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import {
+  TextInput
+} from 'react-native';
+import {View, Text, Button} from 'react-native';
+
+
+function Bot() {
+  const [value, onChangeText] = React.useState('Input test here');
+
+  return ( <
+    TextInput style = {
+      {
+        height: 40,
+        borderColor: 'gray',
+        borderWidth: 1
+      }
+    }
+    onChangeText = {
+      text => onChangeText(text)
+    }
+    value = {
+      value
+    }
+    />
+  );
+  <Button
+          title="Send"
+          onPress={() => Alert.alert('You sent:')}
+        />
+}
+export default Bot;
