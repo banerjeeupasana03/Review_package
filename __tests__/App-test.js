3bbd40247c29b5c5d42a5fc21d909e5536435ccc
Upasana Mukherjee <banerjeeupasana03@gmail.com>
2019-10-11 21:34:15 -0700

    modified Bot js

--- __tests__/App-test.js
+++ __tests__/App-test.js
@@ -4,11 +4,13 @@
 
 import 'react-native';
 import React from 'react';
-import App from '../App';
+import App from '../Comps/App';
+// import Bot from '../Comps/Bot';
 
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
 
 it('renders correctly', () => {
   renderer.create(<App />);
+  // renderer.create(<Bot />);
 });
